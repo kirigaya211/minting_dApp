3578f093901cc3b61ce17b1200011c58a49ee4cf
kirigaya211 <kirigayarz@gmail.com>
2025-04-26 08:12:38 +0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { NFTMintCard } from '@coinbase/onchainkit/nft';
+import { NFTMedia } from '@coinbase/onchainkit/nft/view'; 
+import {
+  NFTCreator, 
+  NFTCollectionTitle, 
+  NFTQuantitySelector, 
+  NFTAssetCost, 
+  NFTMintButton, 
+} from '@coinbase/onchainkit/nft/mint'; 
+
+export default function MintPage() {
+  return (
+    <NFTMintCard contractAddress='0x5E460559B2d57373a5D03CC096FaFFd021CE39F7'>
+      <NFTCreator />
+      <NFTMedia />
+      <NFTCollectionTitle />
+      <NFTQuantitySelector />
+      <NFTAssetCost />
+      <NFTMintButton />
+    </NFTMintCard>
+  );
+}
